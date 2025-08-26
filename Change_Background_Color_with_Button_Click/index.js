a8affd48afdb52689edfb1061183c78244fd5e73
sidra <sidrajagrawal@gmail.com>
2025-08-26 12:33:37 +0530

    Complete

--- Change_Background_Color_with_Button_Click/index.js
+++ Change_Background_Color_with_Button_Click/index.js
@@ -1,27 +1,52 @@
-const body = document.body
+const body = document.body;
 const btnChange = document.querySelector('#btn-change');
 const btnDisco = document.querySelector('#btn-disco');
 const heading = document.querySelector('#heading');
 
-let toggleDisco = false;
+let toggle = true;
+let timerId = null;
 
-btnChange.addEventListener('click', ()=>{
+const randomColorGenerator = () =>{
     const red = Math.floor(Math.random()*255);
-    const blue = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
+    const blue = Math.floor(Math.random()*255);
+    const colorObj = {
+        'red' : red,
+        'blue' : blue,
+        'green' : green,
+    };
+    return colorObj;
+}
+
+
+//Single Change
+btnChange.addEventListener('click',()=>{
+    const res = randomColorGenerator();
+    const {red,blue,green} = res
     if(red == 0 && blue == 0 && green == 0){
         heading.style.color = 'white';
     }
-    body.style.backgroundColor = `rgb(${red},${blue},${green})`
-})
+    body.style.backgroundColor = `rgb(${red},${blue},${green})`;
+});
+
+
+//Disco
 btnDisco.addEventListener('click',()=>{
-    if(!toggleDisco){
+    if(toggle){
         btnDisco.innerText = 'Stop...';
         btnDisco.style.backgroundColor = 'red';
-        toggleDisco = true;
+        toggle = false;
+        timerId = setInterval(()=>{
+            console.log("sid");
+            const res = randomColorGenerator();
+            const {red,blue,green} = res
+            body.style.backgroundColor = `rgb(${red},${blue},${green})`;
+        },100);
     }else{
         btnDisco.innerText = 'Make it Disco';
         btnDisco.style.backgroundColor = 'blueviolet';
-        toggleDisco = false;
+        toggle = true;
+        clearInterval(timerId);
+        timerId = null;
     }
-})
+});
