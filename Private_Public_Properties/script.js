19c46346b42da8457c328e17c3fc26c249332f0a
sidra <sidrajagrawal@gmail.com>
2026-05-25 16:01:02 +0530

    Private Public

---
+++ Private_Public_Properties/script.js
@@ -0,0 +1,21 @@
+// class CreateUser {
+//     #Age;
+//     #Greet = 'Hello'
+//     constructor(FirstName, LastName, Age) {
+//         this.FirstName = FirstName;
+//         this.LastName = LastName;
+//         this.#Age = Age;
+//     }
+
+//     getBirthYear() {
+//         return new Date().getFullYear() - this.#Age;
+//     }
+// }
+
+// const user1 = new CreateUser("Siddharth", "Agrawal", 21);
+
+
+const user = {
+    name: "Sid",
+    '#age': 21
+}
