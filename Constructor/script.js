a42905faec0492ac1b9b973bf44e031f94fbb8a3
sidra <sidrajagrawal@gmail.com>
2026-05-25 14:48:24 +0530

    Updates on Contructor

--- Constructor/script.js
+++ Constructor/script.js
@@ -20,7 +20,7 @@
 // const user2 = createUser("Shyam", "Sharma", 30);
 
 //Here user1 create getBirthYear function in different location and user2 in different location
-//This is not a good Approach because both are common method
+//Not a good Approach because both are common method
 // console.log(user1.getBirthYear === user2.getBirthYear);
 
 //Method 1 to create common method one time(like getBirthYear)
@@ -64,3 +64,7 @@
 
 // const user1 = new CreateUser('Sid', 'Ag', 21);
 // const user2 = new CreateUser('Ram', 'Sharma', 29);
+// console.log(user1.getBirthYear());
+// console.log(user2.getBirthYear());
+// console.log(user1.getFullName());
+// console.log(user2.getFullName());
