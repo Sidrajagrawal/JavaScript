0cccbe6ffe1149f8b34b3f1326468653d5aa02cf
sidra <sidrajagrawal@gmail.com>
2026-05-28 12:29:36 +0530

    Bind Method

---
+++ bind_Method/script.js
@@ -0,0 +1,18 @@
+// function greet() {
+//     console.log(`Hello ${this.fullName}`);
+// }
+
+// const user = {
+//     fullName: "Siddharth"
+// }
+
+// const boundFunction = greet.bind(user);
+
+// const user = {
+//     fullName: "Siddharth",
+//     greet() {
+//         return `Hello ${this.fullName}`;
+//     }
+// }
+// const boundFunction = user.greet.bind(user)
+// console.log(boundFunction());
